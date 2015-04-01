85aa6823fa7312cb4e7c999abb79cd04c2f03b12
George Reyes <zypher452@gmail.com>
2015-03-31 20:20:17 -0700

    Added #4 aka da armor class.

---
+++ YARRR/GGArmor.h
@@ -0,0 +1,16 @@
+//
+//  GGArmor.h
+//  YARRR
+//
+//  Created by George Reyes on 3/31/15.
+//  Copyright (c) 2015 George Reyes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGArmor : NSObject
+
+@property(strong, nonatomic) NSString* name;
+@property(nonatomic) int damageReduction;
+
+@end
---
+++ YARRR/GGArmor.m
@@ -0,0 +1,13 @@
+//
+//  GGArmor.m
+//  YARRR
+//
+//  Created by George Reyes on 3/31/15.
+//  Copyright (c) 2015 George Reyes. All rights reserved.
+//
+
+#import "GGArmor.h"
+
+@implementation GGArmor
+
+@end
