6d824086f310a70ab58cd5b214f5e847a047cd18
George Reyes <zypher452@gmail.com>
2015-03-31 20:14:42 -0700

    This is the beginnings of the weapon class. Should address #3

---
+++ YARRR/GGWeapon.h
@@ -0,0 +1,16 @@
+//
+//  GGWeapon.h
+//  YARRR
+//
+//  Created by George Reyes on 3/31/15.
+//  Copyright (c) 2015 George Reyes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGWeapon : NSObject
+
+@property(strong, nonatomic) NSString* name;
+@property(nonatomic) int damage;
+
+@end
---
+++ YARRR/GGWeapon.m
@@ -0,0 +1,13 @@
+//
+//  GGWeapon.m
+//  YARRR
+//
+//  Created by George Reyes on 3/31/15.
+//  Copyright (c) 2015 George Reyes. All rights reserved.
+//
+
+#import "GGWeapon.h"
+
+@implementation GGWeapon
+
+@end
