baccf5dc566fcf35655145ba2ff1543568b34424
George Reyes <zypher452@gmail.com>
2015-03-31 20:24:03 -0700

    Fixed issue where Character class didn’t have proper import for the the weapon and armor that they pick up.

--- YARRR/GGCharacter.h
+++ YARRR/GGCharacter.h
@@ -7,12 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GGWeapon.h"
+#import "GGArmor.h"
 
 @interface GGCharacter : NSObject
 
 @property(nonatomic ) int health;
 @property(nonatomic ) int damage;
-@property(strong, nonatomic) GGWeapon weapon;
-@property(strong, nonatomic) GGArmor armor;
+@property(strong, nonatomic) GGWeapon* weapon;
+@property(strong, nonatomic) GGArmor* armor;
 
 @end
