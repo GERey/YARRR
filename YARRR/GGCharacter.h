8f112701dd11d77bf21bf7e0e1e4ed10bbb53d86
George Reyes <zypher452@gmail.com>
2015-03-31 20:12:12 -0700

    Added the outline of the character class. This effectively resolves #2

---
+++ YARRR/GGCharacter.h
@@ -0,0 +1,18 @@
+//
+//  GGCharacter.h
+//  YARRR
+//
+//  Created by George Reyes on 3/31/15.
+//  Copyright (c) 2015 George Reyes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGCharacter : NSObject
+
+@property(nonatomic ) int health;
+@property(nonatomic ) int damage;
+@property(strong, nonatomic) GGWeapon weapon;
+@property(strong, nonatomic) GGArmor armor;
+
+@end
---
+++ YARRR/GGCharacter.m
@@ -0,0 +1,12 @@
+//
+//  GGCharacter.m
+//  YARRR
+//
+//  Created by George Reyes on 3/31/15.
+//  Copyright (c) 2015 George Reyes. All rights reserved.
+//
+
+#import "GGCharacter.h"
+
+@implementation GGCharacter
+@end
