8150ef31e0cd2664062ca14450b2d152502a509e
George Reyes <zypher452@gmail.com>
2015-04-07 22:34:43 -0700

    Added direction buttons to view controller. Also added all the properties in the view to the controller so they can be updated.

--- YARRR/GGViewController.h
+++ YARRR/GGViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface GGViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextView *healthDisplay;
+@property (strong, nonatomic) IBOutlet UITextView *damageDisplay;
+@property (strong, nonatomic) IBOutlet UITextView *weaponNameDisplay;
+@property (strong, nonatomic) IBOutlet UITextView *armorNameDisplay;
+@property (strong, nonatomic) IBOutlet UITextView *storyDisplay;
+@property (strong, nonatomic) IBOutlet UIImageView *gameImage;
+- (IBAction)upButton:(UIButton *)sender;
+- (IBAction)leftButton:(UIButton *)sender;
+- (IBAction)rightButton:(UIButton *)sender;
+- (IBAction)downButton:(UIButton *)sender;
 
 @end
